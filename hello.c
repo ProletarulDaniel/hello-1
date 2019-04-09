0cda6dc66a65899a0ad6643aa31e3aeec8dc6a1b
Bogdan <hritcubogdan99@gmail.com>
2019-04-09 13:10:10 +0300

    Saluta pe oricine introduce numele

--- hello.c
+++ hello.c
@@ -2,6 +2,13 @@
 
 int main()
 {
-    printf("Hello, Bogdan!\n");
+    char nume[100];
+    
+    printf("Nume:");
+    
+    scanf("%s", nume);
+    
+    printf("Hello, %s!\n", nume);
+
     return 0;
 }
